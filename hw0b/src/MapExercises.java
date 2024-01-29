b8b8898d6d740dced14a5aab86a09bec93d2c123
Cassie Guo <cassieguo441@gmail.com>
2024-01-28 23:15:11 -0800

    done with hw0b

--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -6,18 +7,39 @@ public class MapExercises {
      * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
      */
     public static Map<Character, Integer> letterToNum() {
-        return null;
+        Map<Character, Integer> res = new HashMap<>();
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        for (char c : alphabet.toCharArray()) {
+            int val = (int)c - 96;
+            res.put(c, val);
+        }
+        return res;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
      *  is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
-        return null;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            int val = num * num;
+            map.put(num, val);
+        }
+        return map;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
-        return null;
+        Map<String, Integer> map = new HashMap<>();
+        for (String w : words) {
+            if (! map.containsKey(w)) {
+                map.put(w, 1);
+            } else {
+                int val = map.get(w);
+                val += 1;
+                map.put(w, val);
+            }
+        }
+        return map;
     }
 }
